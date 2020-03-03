7f0620a57f3c2ccfca45f47492bd05389e706be0
sevenryze <sevenryze@gmail.com>
2020-03-03 23:27:40 +0800

    fix: Export more interfaces

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,3 @@
 export { LoggerModule } from "./LoggerModule/LoggerModule";
 export { LoggerFactory, LoggerFactoryIoCAnchor } from "./LoggerModule/LoggerFactory";
+export { Logger, LoggerLevel } from "./LoggerModule/Logger";
